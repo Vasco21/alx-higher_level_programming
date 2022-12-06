5eaed266cc1a036cae506e6f3470a82eb57116d2
Vasco21 <vascoeti123@gmail.com>
2022-12-05 19:41:40 -0800

    Find the biggest integer of a list

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,7 @@
 # 6-print_matrix_integer.py
 
 def print_matrix_integer(matrix=[[]]):
+
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
                 print("{:d}".format(matrix[i][j]), end="")

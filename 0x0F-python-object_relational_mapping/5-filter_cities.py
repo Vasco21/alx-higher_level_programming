542d44ffa085282b3b0b4e76d956b0095947f7d6
Vasco21 <vascoeti123@gmail.com>
2023-05-14 17:44:10 -0700

    update

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     from the database.
     """
 
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3500,
                          passwd=argv[2], db=argv[3])
 
     with db.cursor() as cur:

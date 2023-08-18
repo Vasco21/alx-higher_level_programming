35e93b9add49a4b960a20e01d4b17328dfae89c2
Vasco21 <vascoeti123@gmail.com>
2023-08-18 15:20:29 -0700

    update

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,17 +1,10 @@
 #!/usr/bin/node
 
 const request = require('request');
-
-// Check if the URL argument is provided
 if (process.argv.length < 3) {
     console.log("Usage: node script.js <URL>");
     process.exit(1);
-}
-
-// Get the URL from the command line argument
 const url = process.argv[2];
-
-// Make a GET request to the specified URL and listen for the 'response' event
 request.get(url).on('response', (response) => {
     console.log(`code: ${response.statusCode}`);
 });

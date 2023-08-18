d6a7f6706a377b92b809f6677a4be67d1b0b7db5
Vasco21 <vascoeti123@gmail.com>
2023-08-18 15:45:07 -0700

    update

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,30 +1,12 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-// Check if the URL argument is provided
-if (process.argv.length < 3) {
-  console.log("Usage: node script.js <API_URL>");
-  process.exit(1);
-}
-
-// Get the API URL from the command line argument
-const apiUrl = process.argv[2];
-
-// Make a request to the API
-request(apiUrl, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
     const results = JSON.parse(body).results;
-
-    // Count the movies where the character with ID ending in "/18/" appears
-    const count = results.reduce((count, movie) => {
+    console.log(results.reduce((count, movie) => {
       return movie.characters.find((character) => character.endsWith('/18/'))
         ? count + 1
         : count;
-    }, 0);
-
-    console.log(count);
-  } else {
-    console.error("Error:", error);
+    }, 0));
   }
 });
--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,17 +1,9 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-// Make a GET request to the specified URL
 request(process.argv[2], function (error, response, body) {
   if (!error) {
-    // Parse the JSON response into an array of todos
     const todos = JSON.parse(body);
-
-    // Create an object to store the completed todo counts for each user
     let completed = {};
-
-    // Iterate through each todo
     todos.forEach((todo) => {
       if (todo.completed && completed[todo.userId] === undefined) {
         completed[todo.userId] = 1;
@@ -19,8 +11,6 @@ request(process.argv[2], function (error, response, body) {
         completed[todo.userId] += 1;
       }
     });
-
-    // Print the completed todo counts
     console.log(completed);
   }
 });

f13acb65167dcbf92c1399c0454b248fdc53748b
Vasco21 <vascoeti123@gmail.com>
2023-08-18 15:39:09 -0700

    update

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,21 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-// Check if the file path argument is provided
-if (process.argv.length < 3) {
-  console.log("Usage: node script.js <file-path>");
-  process.exit(1);
-}
-
-// Get the file path from the command line argument
-const filePath = process.argv[2];
-
-// Read and print the content of the file
-fs.readFile(filePath, 'utf-8', (err, content) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(content);
-  }
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });

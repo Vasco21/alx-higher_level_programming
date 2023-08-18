cacaa0f92d72117dd7a5555b607e5cb746e18dff
Vasco21 <vascoeti123@gmail.com>
2023-08-18 14:58:28 -0700

    update

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 
 // Check if the required arguments are provided
 if (process.argv.length < 4) {
-  console.log("Usage: node script.js <file-path> <content>");
-  process.exit(1);
+	console.log("Usage: node script.js <file-path> <content>");
+	process.exit(1);
 }
 
 // Get the file path and content from the command line arguments
@@ -14,9 +14,9 @@ const content = process.argv[3];
 
 // Write the content to the file
 fs.writeFile(filePath, content, 'utf-8', (err) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log("Content has been written to the file.");
-  }
+	if (err) {
+		console.error(err);
+	} else {
+		console.log("Content has been written to the file.");
+	}
 });

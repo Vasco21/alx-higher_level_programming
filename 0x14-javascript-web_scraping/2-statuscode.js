ce2e618ebac2c0f2008cac34c92929238e07d2b5
Vasco21 <vascoeti123@gmail.com>
2023-08-18 15:22:19 -0700

    update

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
 
 const request = require('request');
+
 if (process.argv.length < 3) {
     console.log("Usage: node script.js <URL>");
     process.exit(1);
+}
+
 const url = process.argv[2];
+
 request.get(url).on('response', (response) => {
     console.log(`code: ${response.statusCode}`);
 });

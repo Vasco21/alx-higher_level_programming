bc247d1a8d8f56758129b926a36aa1f80d251b2f
VascoEti <50703731+Vasco21@users.noreply.github.com>
2023-11-01 03:44:40 +0200

    Delete 0x01-python-if_else_loops_functions directory

--- 0x01-python-if_else_loops_functions/7-islower.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-def islower(c):
-    return True if (ord(c) > 96 and ord(c) < 123) else False
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def print_last_digit(number):
-    n = abs(number) % 10
-    print(n, end='')
-    return n
